8d14251df99df17b5556001f79d9da37513344cb
Tyler arnone <tylerarnone@Tylers-MacBook-Pro-3.local>
2019-02-26 14:07:36 -0600

    phase 2

--- spool/routes/users.js
+++ spool/routes/users.js
@@ -5,5 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/students', usersCtrl.index);
+
+// POST /facts
+// We will already have access to the logged in student on
+// the server, therefore do not use: /students/:id/facts
+router.post('/facts', isLoggedIn,usersCtrl.addFact);
+
+// DELETE /facts/:id
+router.delete('/facts/:id', usersCtrl.delFact);
+
+function isLoggedIn(req, res, next){
+    if ( req.isAuthenticated() ) return next();
+    res.redirect('/auth/google');
+}
 
 module.exports = router;
