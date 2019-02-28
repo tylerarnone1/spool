b128c7fdd7a9789171c1fc60136f53f423d10b1d
Tyler arnone <tylerarnone@Tylers-MacBook-Pro-3.local>
2019-02-27 21:00:16 -0600

    working first view

--- spool/routes/index.js
+++ spool/routes/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
-
-router.get('/', function(req, res) {
-  res.redirect('/users');
+//home page
+router.get('/', function(req, res, next) {
+  res.render('spools/index');
 });
+
+
 
 router.get('/auth/google', passport.authenticate(
   'google',
